2b0dffe4f198fdab77b6580dd12327e477793143
EfremovEgor <howlplaystop@gmail.com>
2025-10-27 13:25:40 +0300

    update 5 files

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [
-		tanstackRouter({
-			target: "react",
-			autoCodeSplitting: true,
-		}),
-		react(),
-		tailwindcss(),
-	],
-	resolve: {
-		alias: {
-			// assets: "/src/assets",
-			"@components": "/src/components",
-			"@routes": "/src/routes",
-			"@api": "/src/api",
-			"@lib": "/src/lib",
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+	return {
+		base: env.VITE_BASE_PATH || "/",
+		plugins: [
+			tanstackRouter({
+				target: "react",
+				autoCodeSplitting: true,
+			}),
+			react(),
+			tailwindcss(),
+		],
+		resolve: {
+			alias: {
+				// assets: "/src/assets",
+				"@components": "/src/components",
+				"@routes": "/src/routes",
+				"@api": "/src/api",
+				"@lib": "/src/lib",
+			},
 		},
-	},
+	};
 });
