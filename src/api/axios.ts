a2bcc6bf6f1aa2bef703996a558a3f7427b00089
EfremovEgor <howlplaystop@gmail.com>
2025-10-28 12:12:27 +0300

    update .gitignore and axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiV1Instance = axios.create({
-	baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+	baseURL: `${import.meta.env.VITE_API_URL}/v1`,
 	timeout: 1000,
 	headers: {
 		"Content-Type": "application/json",
